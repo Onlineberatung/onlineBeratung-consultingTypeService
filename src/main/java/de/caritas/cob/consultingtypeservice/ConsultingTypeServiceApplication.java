5adaa268649846baf6da36863d378fafe31a4ded
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-09-09 12:45:59 +0200

    rollback not required local change

--- src/main/java/de/caritas/cob/consultingtypeservice/ConsultingTypeServiceApplication.java
+++ src/main/java/de/caritas/cob/consultingtypeservice/ConsultingTypeServiceApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsultingTypeServiceApplication {
 
-  static {
-    System.setProperty("os.arch", "i686_64");
-  }
-
   public static void main(String[] args) {
     SpringApplication.run(ConsultingTypeServiceApplication.class, args);
   }
