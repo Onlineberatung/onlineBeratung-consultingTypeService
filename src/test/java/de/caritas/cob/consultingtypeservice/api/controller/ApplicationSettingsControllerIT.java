ef0e682d0083603ec90e37a157eba8dad5b98ab7
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-09-12 15:48:20 +0200

    adding two new settings

--- src/test/java/de/caritas/cob/consultingtypeservice/api/controller/ApplicationSettingsControllerIT.java
+++ src/test/java/de/caritas/cob/consultingtypeservice/api/controller/ApplicationSettingsControllerIT.java
@@ -1,47 +1,15 @@
 package de.caritas.cob.consultingtypeservice.api.controller;
 
-import static de.caritas.cob.consultingtypeservice.testHelper.TopicPathConstants.PATH_GET_PUBLIC_TOPIC_LIST;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import de.caritas.cob.consultingtypeservice.ConsultingTypeServiceApplication;
-import de.caritas.cob.consultingtypeservice.api.auth.UserRole;
-import de.caritas.cob.consultingtypeservice.api.model.ApplicationSettingsEntity;
-import de.caritas.cob.consultingtypeservice.api.model.TopicDTO;
-import de.caritas.cob.consultingtypeservice.api.model.TopicStatus;
 import de.caritas.cob.consultingtypeservice.api.repository.ApplicationSettingsRepository;
 import de.caritas.cob.consultingtypeservice.api.tenant.TenantContext;
-import de.caritas.cob.consultingtypeservice.api.util.JsonConverter;
-import de.caritas.cob.consultingtypeservice.schemas.model.DisableVideoAppointments;
-import de.caritas.cob.consultingtypeservice.schemas.model.EnableTenantTheming;
-import de.caritas.cob.consultingtypeservice.schemas.model.EnableWalkthrough;
-import de.caritas.cob.consultingtypeservice.schemas.model.MultitenancyEnabled;
-import de.caritas.cob.consultingtypeservice.schemas.model.MultitenancyWithSingleDomainEnabled;
-import de.caritas.cob.consultingtypeservice.schemas.model.UseTenantService;
-import de.caritas.cob.consultingtypeservice.testHelper.TopicPathConstants;
-import java.util.Map;
-import org.assertj.core.util.Lists;
-import org.assertj.core.util.Maps;
-import org.assertj.core.util.Sets;
-import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
-import org.keycloak.adapters.spi.KeycloakAccount;
-import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -85,8 +53,6 @@ class ApplicationSettingsControllerIT {
         .andExpect(jsonPath("$.multitenancyWithSingleDomainEnabled.readOnly").value(true))
         .andExpect(jsonPath("$.multitenancyEnabled.value").value(false))
         .andExpect(jsonPath("$.multitenancyEnabled.readOnly").value(true))
-        .andExpect(jsonPath("$.enableTenantTheming.value").value(false))
-        .andExpect(jsonPath("$.enableTenantTheming.readOnly").value(false))
         .andExpect(jsonPath("$.useTenantService.value").value(false))
         .andExpect(jsonPath("$.useTenantService.readOnly").value(false))
         .andExpect(jsonPath("$.enableWalkthrough.value").value(false))
@@ -94,8 +60,11 @@ class ApplicationSettingsControllerIT {
         .andExpect(jsonPath("$.disableVideoAppointments.value").value(true))
         .andExpect(jsonPath("$.disableVideoAppointments.readOnly").value(false))
         .andExpect(jsonPath("$.mainTenantSubdomainForSingleDomainMultitenancy.value").value("app"))
-        .andExpect(jsonPath("$.mainTenantSubdomainForSingleDomainMultitenancy.readOnly").value(false));
-
+        .andExpect(jsonPath("$.mainTenantSubdomainForSingleDomainMultitenancy.readOnly").value(false))
+        .andExpect(jsonPath("$.budibaseSSO.value").value(false))
+        .andExpect(jsonPath("$.budibaseSSO.readOnly").value(false))
+        .andExpect(jsonPath("$.useOverviewPage.value").value(false))
+        .andExpect(jsonPath("$.useOverviewPage.readOnly").value(false));
   }
 
 }
