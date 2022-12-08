792e9f57e4e143acd61bc99e9e750c31c69d4dae
idriss.naji <idriss.naji@virtual-identity.com>
2022-12-08 10:41:56 +0100

    fix: increase name and description column size

--- src/main/resources/db/changelog/changeset/0003_migrate_topic_to_multilingual_structure/migrateToMultilingualTopic.sql
+++ src/main/resources/db/changelog/changeset/0003_migrate_topic_to_multilingual_structure/migrateToMultilingualTopic.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `consultingtypeservice`.`topic`
-    MODIFY `name` varchar(225) COLLATE 'utf8_unicode_ci' NULL,
-    MODIFY `description` varchar(225) COLLATE 'utf8_unicode_ci' NULL;
+    MODIFY `name` varchar(1024) COLLATE 'utf8_unicode_ci' NULL,
+    MODIFY `description` varchar(1024) COLLATE 'utf8_unicode_ci' NULL;
 
 UPDATE `consultingtypeservice`.`topic`
 SET name = concat('{"de": "', name, '"}');
