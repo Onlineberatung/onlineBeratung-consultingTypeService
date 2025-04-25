6062a0151bfb49b07d3a41f8960362b34ddfe0a9
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-04-25 15:01:52 +0200

    fix: tests

--- src/test/java/de/caritas/cob/consultingtypeservice/api/consultingtypes/ConsultingTypeGroupRepositoryIT.java
+++ src/test/java/de/caritas/cob/consultingtypeservice/api/consultingtypes/ConsultingTypeGroupRepositoryIT.java
@@ -1,6 +1,7 @@
 package de.caritas.cob.consultingtypeservice.api.consultingtypes;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import de.caritas.cob.consultingtypeservice.ConsultingTypeServiceApplication;
 import de.caritas.cob.consultingtypeservice.schemas.model.ConsultingType;
 import de.caritas.cob.consultingtypeservice.testHelper.MongoTestInitializer;
@@ -41,24 +42,20 @@ class ConsultingTypeGroupRepositoryIT {
     final int CONSULTING_TYPE_ID_1 = 1;
     final int CONSULTING_TYPE_ID_2 = 2;
 
-    assertThat(result).isNotNull()
-        .containsKeys(GROUP_1, GROUP_2, GROUP_3);
+    assertThat(result).isNotNull().containsKeys(GROUP_1, GROUP_2, GROUP_3);
 
     assertThat(result.get(GROUP_1))
         .hasSize(1)
         .extracting("id")
         .containsExactly(CONSULTING_TYPE_ID_0);
 
-    assertThat(result.get(GROUP_1).get(0).getGroups())
-        .contains(GROUP_1);
+    assertThat(result.get(GROUP_1).get(0).getGroups()).contains(GROUP_1);
 
     assertThat(result.get(GROUP_2))
         .hasSize(2)
         .extracting("id")
         .containsExactlyInAnyOrder(CONSULTING_TYPE_ID_1, CONSULTING_TYPE_ID_2);
 
-    assertThat(result.get(GROUP_3))
-        .extracting("id")
-        .contains(CONSULTING_TYPE_ID_1);
+    assertThat(result.get(GROUP_3)).extracting("id").contains(CONSULTING_TYPE_ID_1);
   }
 }
