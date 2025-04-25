1b4175e175ce9cc0be6d00521f1daba8dc8ce8c7
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-04-25 11:13:19 +0200

    fix: tests

--- src/test/java/de/caritas/cob/consultingtypeservice/api/consultingtypes/ConsultingTypeGroupRepositoryIT.java
+++ src/test/java/de/caritas/cob/consultingtypeservice/api/consultingtypes/ConsultingTypeGroupRepositoryIT.java
@@ -1,18 +1,11 @@
 package de.caritas.cob.consultingtypeservice.api.consultingtypes;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import de.caritas.cob.consultingtypeservice.ConsultingTypeServiceApplication;
 import de.caritas.cob.consultingtypeservice.schemas.model.ConsultingType;
 import de.caritas.cob.consultingtypeservice.testHelper.MongoTestInitializer;
 import java.util.List;
 import java.util.Map;
-import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,29 +27,38 @@ class ConsultingTypeGroupRepositoryIT {
   @Test
   void getConsultingTypesGroupMap_Should_ReturnMapWithConsultingTypeGroups() {
 
+    // when
     Map<String, List<ConsultingType>> result =
         consultingTypeGroupRepository.getConsultingTypesGroupMap();
 
-    assertThat(result, notNullValue());
+    // then
+
     final String GROUP_1 = "group1";
-    final int CONSULTING_TYPE_ID_0 = 0;
-    assertThat(result.get(GROUP_1), hasSize(1));
-    assertThat(result.get(GROUP_1).get(0).getId(), is(CONSULTING_TYPE_ID_0));
-    assertThat(result.get(GROUP_1).get(0).getGroups().contains(GROUP_1), is(true));
     final String GROUP_2 = "group2";
     final String GROUP_3 = "group3";
+
+    final int CONSULTING_TYPE_ID_0 = 0;
     final int CONSULTING_TYPE_ID_1 = 1;
     final int CONSULTING_TYPE_ID_2 = 2;
-    assertThat(result.get(GROUP_2), hasSize(2));
-    assertThat(result, IsMapContaining.hasKey(GROUP_2));
-    assertThat(result, IsMapContaining.hasKey(GROUP_3));
-    assertThat(result, IsMapContaining.hasKey(GROUP_1));
-    assertThat(result.get(GROUP_1).get(0).getId(), is(CONSULTING_TYPE_ID_0));
-    assertThat(
-        result.get(GROUP_2),
-        containsInAnyOrder(
-            hasProperty("id", is(CONSULTING_TYPE_ID_1)),
-            hasProperty("id", is(CONSULTING_TYPE_ID_2))));
-    assertThat(result.get(GROUP_3).get(0).getId(), is(CONSULTING_TYPE_ID_1));
+
+    assertThat(result).isNotNull()
+        .containsKeys(GROUP_1, GROUP_2, GROUP_3);
+
+    assertThat(result.get(GROUP_1))
+        .hasSize(1)
+        .extracting("id")
+        .containsExactly(CONSULTING_TYPE_ID_0);
+
+    assertThat(result.get(GROUP_1).get(0).getGroups())
+        .contains(GROUP_1);
+
+    assertThat(result.get(GROUP_2))
+        .hasSize(2)
+        .extracting("id")
+        .containsExactlyInAnyOrder(CONSULTING_TYPE_ID_1, CONSULTING_TYPE_ID_2);
+
+    assertThat(result.get(GROUP_3))
+        .extracting("id")
+        .contains(CONSULTING_TYPE_ID_1);
   }
 }
